bdec6cf4a394700e20a6b67881409d1098ceabc0
Ethan Dalool <git@voussoir.net>
2018-04-19 19:29:06 -0700

    Add filter to generate /file/ link with appropriate basename.
    Because inlining the |urlencode every time is easy to forget.

--- frontends/etiquette_flask/etiquette_flask/common.py
+++ frontends/etiquette_flask/etiquette_flask/common.py
@@ -33,6 +33,7 @@ site.jinja_env.add_extension('jinja2.ext.do')
 site.jinja_env.trim_blocks = True
 site.jinja_env.lstrip_blocks = True
 site.jinja_env.filters['bytestring'] = jinja_filters.bytestring
+site.jinja_env.filters['file_link'] = jinja_filters.file_link
 site.debug = True
 
 P = etiquette.photodb.PhotoDB()
--- frontends/etiquette_flask/etiquette_flask/jinja_filters.py
+++ frontends/etiquette_flask/etiquette_flask/jinja_filters.py
@@ -1,9 +1,16 @@
 import etiquette
+import jinja2.filters
 
 import voussoirkit.bytestring
 
 def bytestring(x):
     try:
         return voussoirkit.bytestring.bytestring(x)
-    except Exception:
+    except Exception as e:
         return '??? b'
+
+def file_link(photo, short=False):
+    if short:
+        return f'/file/{photo.id}{photo.dot_extension}'
+    basename = jinja2.filters.do_urlencode(photo.basename)
+    return f'/file/{photo.id}/{basename}'
