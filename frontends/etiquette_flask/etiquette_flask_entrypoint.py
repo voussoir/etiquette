88a6708061ccdc7d3ac4d031c48de44039937b54
Ethan Dalool <git@voussoir.net>
2020-05-17 16:33:40 -0700

    Replace werkzeug.contrib with werkzeug.middleware proxyfix.
    werkzeug.contrib has been deprecated, this is the new location
    of the proxyfix.

--- frontends/etiquette_flask/etiquette_flask_entrypoint.py
+++ frontends/etiquette_flask/etiquette_flask_entrypoint.py
@@ -4,10 +4,10 @@ This file is the WSGI entrypoint for remote / production use.
 If you are using Gunicorn, for example:
 gunicorn etiquette_flask_entrypoint:site --bind "0.0.0.0:PORT" --access-logfile "-"
 '''
-import werkzeug.contrib.fixers
+import werkzeug.middleware.proxy_fix
 
 import backend
 
-backend.site.wsgi_app = werkzeug.contrib.fixers.ProxyFix(backend.site.wsgi_app)
+backend.site.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(backend.site.wsgi_app)
 
 site = backend.site
